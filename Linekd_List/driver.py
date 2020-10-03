a23de1a792a65aa488a36ff161314afd9710e294
Kunal Gupta <kunalgupta2k12@gmail.com>
2020-10-04 01:44:23 +0530

    More practice on Singly Linked List

--- Linekd_List/driver.py
+++ Linekd_List/driver.py
@@ -1,9 +1,10 @@
 from Linekd_List.linked_list import LinkedList
 
 llist = LinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
+llist.append(1)
+llist.append(2)
+llist.append(3)
+
 
 llist.insert_after_node(llist.head.next, "D")
 
@@ -13,3 +14,16 @@ llist.delete_node("E")
 llist.delete_node_at_pos(0)
 
 llist.print_list()
+
+print(llist.len_iterative())
+
+llist2 = LinkedList()
+llist2.append(4)
+llist2.append(5)
+llist2.append(6)
+
+print(llist.sum_two_lists(llist2))
+
+
+
+
